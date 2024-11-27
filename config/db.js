6f931c4254b4d2dbb6f4e94f92fa647e76fa9ae2
minh-truong-081003 <21110339@student.hcmute.edu.vn>
2024-11-27 20:12:00 +0700

    add mongodb local

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI || process.env.MONGO_URI_LOCAL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
